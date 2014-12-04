8f1eca621c9e7bcabed4695f0781f53f54e22c75
Venky Mullapudi <something.venky@gmail.com>
2014-12-04 14:56:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CSC DoCreator</title>
+<title>Easy Document Creator</title>
 <link id="page_favicon" href="images/favicon.ico" rel="icon" type="image/x-icon" />
 <link href="CSS/style.css" rel="stylesheet" type="text/css" />
 </head>
